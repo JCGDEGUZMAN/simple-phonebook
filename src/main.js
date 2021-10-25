4069c4d4899ea467b8fa12fbd8b570b49a7845ed
JCGDEGUZMAN <julius.d@cloudwalkdigital.com>
2021-10-25 11:41:25 +0800

    add bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-let app = new Vue({
+new Vue({
   render: h => h(App),
-})
+}).$mount('#app')
 
-app.$mount('#app')
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
